e2f22550912cfdd61a4fdd84aa1515489c277521
jtkyber <43868043+jtkyber@users.noreply.github.com>
2023-03-09 16:14:08 -0500

    added 'mongoose.set('strictQuery', true)'

--- connectDB.ts
+++ connectDB.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectMongo = async () => {
     if (process.env.MONGODB_URI)
     {
+        mongoose.set('strictQuery', true);
         mongoose.connect(process.env.MONGODB_URI, {
             autoIndex: true
         })
--- environment.d.ts
+++ environment.d.ts
@@ -1,8 +1,12 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
+            MONGODB_URI: string
+            MONGODB_DB: string
             GOOGLE_CLIENT_ID: string
             GOOGLE_CLIENT_SECRET: string
+            CURRENT_BASE_URL: string
+            AUTH_SECRET: string
         }
     }
 }
