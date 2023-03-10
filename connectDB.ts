8556cd5d76572498535cccd6e3fa6321ada38b6a
jtkyber <43868043+jtkyber@users.noreply.github.com>
2023-03-09 19:54:18 -0500

    fix

--- connectDB.ts
+++ connectDB.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const connectMongo = async () => {
-    console.log(process.env.MONGODB_URI)
     if (process.env.MONGODB_URI)
     {
         mongoose.set('strictQuery', true);
-        mongoose.connect(process.env.MONGODB_URI, {
+        const success = mongoose.connect(process.env.MONGODB_URI, {
             autoIndex: true
         })
+        console.log(success)
     }
 }
 
