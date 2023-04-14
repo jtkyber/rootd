84ddeca098e25d3856608bbc381d5c1cd8aa0c8e
jtkyber <43868043+jtkyber@users.noreply.github.com>
2023-04-13 20:56:54 -0400

    only show group details if group selected / group details btn css animation changes

--- components/GroupDetails.tsx
+++ components/GroupDetails.tsx
@@ -19,34 +19,36 @@ const GroupDetails = ({ selectedGroup, username, onlineMembers }: { selectedGrou
     }
 
     return (
-        <div className={`${styles.groupDetails} ${detailedExpanded ? styles.show : null}`}>
-            <div className={styles.sectionOne}>
-                <h2 className={styles.name}>{selectedGroup.name}</h2>
-                <h5 className={styles.summary}>{selectedGroup.summary}</h5>
-            </div>
+            selectedGroup?._id ?
+                <div className={`${styles.groupDetails} ${detailedExpanded ? styles.show : null}`}>
+                    <div className={styles.sectionOne}>
+                        <h2 className={styles.name}>{selectedGroup.name}</h2>
+                        <h5 className={styles.summary}>{selectedGroup.summary}</h5>
+                    </div>
 
-            <div className={styles.sectionTwo}>
-                <h3>Members</h3>
-                <div className={styles.members}>
-                    {
-                        selectedGroup.members.map((member, i) => (
-                            <h5 className={`${styles.member} ${ onlineMembers.includes(member) ? styles.inGroup : null}`} key={i}>{member}</h5>
-                        ))
-                    }
+                    <div className={styles.sectionTwo}>
+                        <h3>Members</h3>
+                        <div className={styles.members}>
+                            {
+                                selectedGroup.members.map((member, i) => (
+                                    <h5 className={`${styles.member} ${ onlineMembers.includes(member) ? styles.inGroup : null}`} key={i}>{member}</h5>
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                    <div className={styles.bottomSection}>
+                        <button onClick={leaveGroup} className={styles.leaveGroup}>Leave Group</button>
+                        {username === selectedGroup.groupAdmin 
+                        ? <button className={styles.removeGroup}>Remove Group</button>
+                        : null}
+                    </div>
+
+                    <button onClick={() => setDetailedExpanded(!detailedExpanded)} className={`${styles.expandGroupDetailsBtn} ${detailedExpanded ? styles.show : null}`}>
+                        <GroupDetailsArrow />
+                    </button>
                 </div>
-            </div>
-
-            <div className={styles.bottomSection}>
-                <button onClick={leaveGroup} className={styles.leaveGroup}>Leave Group</button>
-                {username === selectedGroup.groupAdmin 
-                ? <button className={styles.removeGroup}>Remove Group</button>
-                : null}
-            </div>
-
-            <button onClick={() => setDetailedExpanded(!detailedExpanded)} className={`${styles.expandGroupDetailsBtn} ${detailedExpanded ? styles.show : null}`}>
-                <GroupDetailsArrow />
-            </button>
-        </div>
+            : null
     )
 }
 
