31709f5880a8c51994ed72d87227cf5d87966462
jtkyber <43868043+jtkyber@users.noreply.github.com>
2023-05-15 13:40:49 -0400

    fixed not authorized screen color

--- components/NotAuthorizedScreen.tsx
+++ components/NotAuthorizedScreen.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { IUserState } from '../redux/userSlice'
+import { useAppSelector } from '../redux/hooks'
 
 const NotAuthorizedScreen = () => {
+    const user: IUserState = useAppSelector(state => state.user)
+
     const style:React.CSSProperties = {
         position: 'absolute',
         width: '100%',
         height: '100%',
-        color: '#1b0d09',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center'
@@ -13,7 +16,7 @@ const NotAuthorizedScreen = () => {
 
     return (
         <div style={style}>
-            <h1 style={{ fontWeight: '600' }}>Not Authorized</h1>
+            <h1 style={{ fontWeight: '600', color: user?.darkMode === true ? 'rgb(206, 204, 204' : '#1b0d09' }}>Not Authorized</h1>
         </div>
     )
 }
